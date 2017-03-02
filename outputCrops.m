5454e7aab75cd4dcbffd616de69ad1e3adcb1ab5
Hirsh Agarwal <hirsh.agarwal@gmail.com>
2017-03-02 20:21:19 +0000

    Update

--- outputCrops.m
+++ outputCrops.m
@@ -15,6 +15,8 @@ function y = outputCrops(handles)
         cropList = [cropList; xVal,yVal];
     end
 
+    
+    
     % The crop list can now be used to generate crops
     
     % Get the number of files to crop
@@ -32,6 +34,23 @@ function y = outputCrops(handles)
         curFile = curFile(1:end-4);
         info = imfinfo(impath);
         num_images = numel(info);
+        
+        % Setup Excel Output
+        filename = strcat(outPath,'CropData.xlsx');
+        xlData = {'Crop', 'Case', 'Block', 'Image', 'Notes'};
+
+        % Parse File Name
+        % curFile = SD014-15 BA41-42-b1 Alz50-647 image1 aligned
+        brokenPath = strsplit(curFile);
+        imCase = brokenPath{3};
+        [startIndexImage, endIndexImage] = regexp(curFile, 'image\d.* ');
+        imageNum = curFile(startIndexImage:endIndexImage);
+        imageNum = imageNum(regexp(imageNum,'\d'):end);
+        imBlockSection = strsplit(brokenPath{2}, '-');
+        imBlock = imBlockSection{3};
+        imBlock = imBlock(regexp(imBlock, '\d'):end);
+        
+        % Crop the images
         for j=1:1:size(cropList, 1)
             for k=1:1:num_images
                 image = imread(impath, k);
@@ -45,18 +64,17 @@ function y = outputCrops(handles)
                 imshow(crop);
                 imwrite(crop, outputPath, 'writemode', 'append');
             end
+            newData = {currentImageNum, imCase, imBlock, imageNum, ''};
+            newVar = vertcat(xlData, newData);
+            xlData = newVar;
             currentImageNum = currentImageNum + 1;
         end
    % end
-    
-    set(handles.infoText, 'string', 'Done');
+   
     disp('Cropping Complete');
-    filename = 'Crop Data.xlsx';
-    xlData = {'Crop', 'Case', 'Block', 'Image', 'Notes'};
+    set(handles.infoText, 'string', 'Writing Excel File');
+    xlswrite(filename,xlData);
     
-    for i=startNum:1:num_images
-        xlData = {xlData; i, '', '', '', ''};
-    end
-    
-    xlswrite(filename,A);
+    disp('Excel File Written');
+    set(handles.infoText, 'string', 'Done');
 end
